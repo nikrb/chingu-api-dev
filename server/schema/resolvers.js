24219d5f0646ecb3f55405db7756135daf1fd6a1
nikrb <nik_rb@yahoo.com>
2018-07-13 23:31:16 +0100

    remove old graphql example code

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -43,18 +43,6 @@ const resolvers = (db) => {
         return id2String(res[0]);
       },
       countries: async () => (await Countries.find({}).toArray()).map(id2String),
-      post: async (root, { _id }) =>
-        id2String(await Posts.findOne(ObjectId(_id))),
-      posts: async () => (await Posts.find({}).toArray()).map(id2String),
-      comment: async (root, { _id }) =>
-        id2String(await Comments.findOne(ObjectId(_id))),
-    },
-    Post: {
-      comments: async({ _id }) =>
-        (await Comments.find({postId: _id}).toArray()).map(id2String),
-    },
-    Comment: {
-      post: async({ postId }) => id2String(await Posts.findOne(ObjectId(_id)))
     },
     Mutation: {
       createStandup: async (root, args) => {
@@ -81,14 +69,6 @@ const resolvers = (db) => {
         Countries.updateOne({ _id: ObjectId(new_id)}, {$set: { id: new_id }});
         return id2String(res.ops[0]);
       },
-      createPost: async (root, args, context, info) => {
-        const res = await Posts.insert(args);
-        return id2String(await Posts.findOne({ _id: res.insertedIds[0]}));
-      },
-      createComment: async (root, args) => {
-        const res = await Comments.insert(args);
-        return id2String(await Comments.findOne({ _id: res.insertedIds[0]}));
-      }
     }
   };
 };
--- server/schema/typedefs.js
+++ server/schema/typedefs.js
@@ -6,9 +6,6 @@ const typeDefs = `
     user(username: String): User
     users: [User]
     standups(username: String): [Standup]
-    post(_id: String): Post
-    posts: [Post]
-    comment(_id: String): Comment
   }
   type User {
     _id: String
@@ -33,18 +30,6 @@ const typeDefs = `
     id: ID
     name: String
   }
-  type Post {
-    _id: String
-    title: String
-    content: String
-    comments: [Comment]
-  }
-  type Comment {
-    _id: String
-    postId: String
-    content: String
-    post: Post
-  }
   input CountryInput{
     name: String!
   }
@@ -65,8 +50,6 @@ const typeDefs = `
     createStandup(standup_data: StandupInput!): Standup
     createUser(user_data: UserInput!): User
     createCountry(country: CountryInput!): Country
-    createPost(title: String, content: String): Post
-    createComment(postId:String, content: String): Comment
   }
   schema {
     query: Query
